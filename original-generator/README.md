9bf6ed992440c3fdc13f919fb3c92a7443522e62
刘章盛 <zhangsheng.liu@niumowang-inc.com>
2019-06-16 19:35:40 +0800

    增加原生的mybatis生成工具

---
+++ original-generator/README.md
@@ -0,0 +1,4 @@
+#original-generator
+
+使用原生的生成工具用命令的方式生成，命令为：
+java -jar mybatis-generator-core-1.3.2.jar -configfile generator.xml -overwrite
---
+++ original-generator/src/main/java/com/softwareloop/mybatis/generator/plugins/MyMapper.java
@@ -0,0 +1,13 @@
+package com.softwareloop.mybatis.generator.plugins;
+
+//import tk.mybatis.mapper.common.Mapper;
+//import tk.mybatis.mapper.common.MySqlMapper;
+//
+///**
+// * 通用mapper
+// *
+// * @author liuzhangsheng
+// * @create 2019/3/13
+// */
+//public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+//}
